557eac954756b16fab27da4f91b88a82f1ea3fd8
sinan urgun <77778835+SinanUrgunWork@users.noreply.github.com>
2023-06-09 16:32:37 +0300

    WebBaseSiteScraper

---
+++ WebBaseSiteScraper/mysite/myapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Link(models.Model):
+    name = models.CharField(max_length=1000, null=True, blank=True)
+    address = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
